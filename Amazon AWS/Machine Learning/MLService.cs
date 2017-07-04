278dd120cb87b950c3dd4320e64fca90ec296987
Scott Josephson <gedman4b@yahoo.com>
2017-07-04 00:19:41 -0400

    Add files via upload

---
+++ Amazon AWS/Machine Learning/MLService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+using MLDataLayer;
+
+namespace MLServiceLayer
+{
+    public static class MLService
+    {
+        public static void GetDataSource()
+        {
+            MLDataSource mLDS = new MLDataSource();
+            byte[] fileBytes = mLDS.GetS3File(ConfigurationManager.AppSettings["bucketName"], "9f0c45b8-044d-430a-900e-286c4e58977a");
+        }   
+    }
+}
